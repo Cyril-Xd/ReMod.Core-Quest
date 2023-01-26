2d378881738613bde7ae6c34b11209c8a355b9f5
uwuclara <31383052+uwuclara@users.noreply.github.com>
2023-01-26 12:43:28 +0500

    fix got blank page

--- Managers/UiManager.cs
+++ Managers/UiManager.cs
@@ -12,23 +12,23 @@ namespace ReMod.Core.Managers
 
         public UiManager(string menuName, Sprite menuSprite, bool createTargetMenu = true, bool createMainMenu = true, bool crxcmodule = false)
         {
-            if (crxcmodule == false)
+            if (!crxcmodule)
             {
                 MainMenu = new ReMenuPage(menuName, true);
                 ReTabButton.Create(menuName, $"Open the {menuName} menu.", menuName, menuSprite);
             }
-            if (crxcmodule = true)
+            if (crxcmodule)
             {
                 var localMenu = new ReMenuPage(QuickMenuEx.CRXCMenu.transform);
                 MainMenu = localMenu.AddMenuPage($"{menuName}");
             }
 
-            if (createTargetMenu == true)
+            if (createTargetMenu)
             {
                 var localMenu = new ReCategoryPage(QuickMenuEx.SelectedUserLocal.transform);
                 TargetMenu = localMenu.AddCategory($"{menuName}");
             }
-            if (createMainMenu == true)
+            if (createMainMenu )
             {
                 var localMenu = new ReCategoryPage(QuickMenuEx.DashboardMenu.transform);
                 LaunchPad = localMenu.AddCategory($"{menuName}");
