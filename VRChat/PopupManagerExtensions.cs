220ff4289926416360f8818f7b91c456e73eebc7
uwuclara <uwuclara@uwuclara.dev>
2023-03-25 01:08:19 +0100

    added more popups

--- VRChat/PopupManagerExtensions.cs
+++ VRChat/PopupManagerExtensions.cs
@@ -18,12 +18,28 @@ namespace ReMod.Core.VRChat
             else
                 MenuEx.Instance.Method_Public_Void_String_String_String_String_Action_Action_String_TextAlignmentOptions_0(title, content, leftBtnText, rightBtnText, leftBtnAction, rightBtnAction);
         }
+        
+        public static void Alert3Box(string title, string content, Action leftBtnAction = null, Action middleBtnAction = null, Action rightBtnAction = null, string leftBtnText = "Yes", string middleBtnText = "Maybe", string rightBtnText = "No", bool showInMainMenu = false)
+        {
+            if (showInMainMenu)
+                MenuEx.MMInstance.Method_Public_Void_String_String_String_String_String_Action_Action_Action_String_TextAlignmentOptions_PDM_0(title, content, leftBtnText, middleBtnText, rightBtnText, leftBtnAction, middleBtnAction, rightBtnAction);
+            else
+                MenuEx.Instance.Method_Public_Void_String_String_String_String_String_Action_Action_Action_String_TextAlignmentOptions_PDM_0(title, content, leftBtnText, middleBtnText, rightBtnText, leftBtnAction, middleBtnAction, rightBtnAction);
+        }
 
         public static void MainMenuBigAlert(string title, string content)
         {
             MenuEx.MMInstance.Method_Public_Void_String_String_PDM_0(title, content);
         }
-        
+
+        public static void InfoBox(string content, bool showInMainMenu = false)
+        {
+            if (showInMainMenu)
+                MenuEx.MMInstance.Method_Public_Virtual_Final_New_Void_String_0(content);
+            else
+                MenuEx.Instance.Method_Public_Virtual_Final_New_Void_String_0(content);
+        }
+
         public enum KeyBoardType
         {
             Standard = EnumPublicSealedvaStNuSe4vUnique.Standard,
@@ -31,7 +47,21 @@ namespace ReMod.Core.VRChat
             Search = EnumPublicSealedvaStNuSe4vUnique.Search
         }
 
-        public static void InputPopup(string Title, Action<string> EndString, Action<string> RealTimeString = null, Action OnClose = null, KeyBoardType keyBoardType = KeyBoardType.Standard, string Placeholder = "Enter Text", string OkButton = "OK", bool MultiLine = true, int CharLimit = 0)
+        public static void InputPopup
+        (
+            string Title, 
+            Action<string> EndString, 
+            Action<string> RealTimeString = null, 
+            Action OnClose = null, 
+            KeyBoardType keyBoardType = KeyBoardType.Standard, 
+            string Placeholder = "Enter Text", 
+            string OkButton = "OK", 
+            string CancelButton = "Cancel", 
+            bool MultiLine = true, 
+            int CharLimit = 0,
+            bool KeepOpen = false,
+            bool ReadOnly = false
+        )
         {
             if (_KeyboardComponent == null)
             {
@@ -40,13 +70,17 @@ namespace ReMod.Core.VRChat
                 _KeyboardComponent = keyboardGameObject.AddComponent<VRCInputField>();
             }
 
+            // No idea what these do as I don't see any effect changing it
+            const bool unKnownBool = true;
+            const string stringThatDoesNothing = "";
+            
             try
             {
                 KeyboardData One = new();
-                KeyboardData Two = One.Method_Public_KeyboardData_String_String_String_String_String_0(Title, Placeholder, "3", OkButton);
-                KeyboardData Three = Two.Method_Public_KeyboardData_Action_1_String_Action_1_String_Action_Boolean_0(RealTimeString, EndString, OnClose, false);
-                KeyboardData Four = Three.Method_Public_KeyboardData_EnumPublicSealedvaStNuSe4vUnique_Boolean_0((EnumPublicSealedvaStNuSe4vUnique) keyBoardType, true);
-                KeyboardData Five = Four.Method_Public_KeyboardData_InputType_ContentType_Int32_Boolean_Boolean_InterfacePublicAbstractBoStVoAc1VoAcSt1BoUnique_0(TMP_InputField.InputType.Standard, TMP_InputField.ContentType.Standard, CharLimit, MultiLine, false, null);
+                KeyboardData Two = One.Method_Public_KeyboardData_String_String_String_String_String_0(Title, Placeholder, stringThatDoesNothing, OkButton, CancelButton);
+                KeyboardData Three = Two.Method_Public_KeyboardData_Action_1_String_Action_1_String_Action_Boolean_0(RealTimeString, EndString, OnClose, KeepOpen);
+                KeyboardData Four = Three.Method_Public_KeyboardData_EnumPublicSealedvaStNuSe4vUnique_Boolean_0((EnumPublicSealedvaStNuSe4vUnique) keyBoardType, unKnownBool);
+                KeyboardData Five = Four.Method_Public_KeyboardData_InputType_ContentType_Int32_Boolean_Boolean_InterfacePublicAbstractBoStVoAc1VoAcSt1BoUnique_0(TMP_InputField.InputType.Standard, TMP_InputField.ContentType.Standard, CharLimit, MultiLine, ReadOnly, null);
                 Five._isWorldKeyboard = true;
                 _KeyboardComponent._keyboardData = Five;
                 _KeyboardComponent.Method_Private_Void_0();
